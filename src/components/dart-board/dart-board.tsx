3e5810cd29c70bb44b360ff19b56f3ede52998b2
Krzysztof Kasprzak <Krzysztof.Kasprzak@comparethemarket.com>
2023-02-05 11:25:53 +0000

    :construction: Working on adding the board numbers

--- src/components/dart-board/dart-board.tsx
+++ src/components/dart-board/dart-board.tsx
@@ -7,6 +7,9 @@ import {Component, h} from "@stencil/core";
 })
 export class DartBoard {
   render () {
-    return <div>Very little!</div>
+    const boardScores: number[] = [20, 1, 18, 4, 13, 6, 15, 2, 17, 3, 19, 7, 16, 8, 11, 14, 9, 12, 5];
+    return [
+      <button id="outer-rim"><ul>{boardScores.map(s => <li id={`s${s}`}>{s}</li>)}</ul></button>
+    ]
   }
 }
