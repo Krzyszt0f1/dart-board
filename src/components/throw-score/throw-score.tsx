74db43c3a64f9a6436a676cc3bc28adc1af6e5b6
Krzysztof Kasprzak <Krzysztof.Kasprzak@comparethemarket.com>
2023-04-03 16:42:46 +0100

    :lipstick: Not perfect, but even has an almost nice score delay

--- src/components/throw-score/throw-score.tsx
+++ src/components/throw-score/throw-score.tsx
@@ -7,14 +7,24 @@ import {Component, h, Listen, State} from "@stencil/core";
 })
 export  class ThrowScore {
   @State() score = 0;
+  @State() scoreStyleId = "";
 
   @Listen('throwValueSelected', {target: 'body'})
   onStockSymbolSelect(event: CustomEvent) {
-    console.log(event.detail)
     this.score = event.detail;
+    this.scoreStyleId = "anim";
+    setTimeout(() => {
+      this.scoreStyleId = "";
+    }, 2000);
   }
 
   render () {
-    return <h1>This throw is worth: {this.score}</h1>
+    return(
+      <div id="container">
+        <div id="frame">
+          <h1>This throw is worth <span id={this.scoreStyleId}>{this.score}</span></h1>
+        </div>
+      </div>
+      )
   }
 }
